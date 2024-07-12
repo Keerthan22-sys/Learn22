b86e632df3daa1b2a04cdb1168a474fcfa7a8531
Keerthan <keerthangowdas222@gmail.com>
2024-07-12 23:03:48 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -14,3 +14,34 @@ void main() {
     ),
   )));
 }
+
+// import 'package:flutter/material.dart';
+
+// void main() => runApp(HelloFlutter());
+
+// class HelloFlutter extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//         //uncomment the debug
+//         debugShowCheckedModeBanner: false,
+//         home: Scaffold(
+//             backgroundColor: Colors.red,
+//             appBar: AppBar(
+//               title: const Text('Hello Flutter'),
+//               leading: const Icon(Icons.home),
+//               actions: [
+//                 IconButton(
+//                   icon: const Icon(Icons.refresh),
+//                   onPressed: () {},
+//                 )
+//               ],
+//             ),
+//             body: const Center(
+//                 child: Text(
+//               'Hello Flutter',
+//               style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+//             ))));
+//   }
+// }
+
